8b8f22af5b7b5fec5b07add379e953d3b3a38a87
ff3f1 <ff3f1@yandex.ru>
2026-07-29 19:49:20 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://cz-adaptech.github.io/gmaven") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
