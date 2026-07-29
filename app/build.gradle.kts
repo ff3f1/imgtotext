20c89501bd5d0d46f50dd6e64b5b3e1bc8e5050a
ff3f1 <ff3f1@yandex.ru>
2026-07-29 18:32:29 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation("cz.adaptech:tesseract4android:4.7.0")
+    implementation("cz.adaptech:tesseract4android:4.7.0") {
+        exclude(group = "cz.adaptech", module = "tesseract4android-openmp")
+    }
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
