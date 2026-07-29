e7e209bdd2bb8520513811dd552d68481f652908
ff3f1 <ff3f1@yandex.ru>
2026-07-30 00:28:31 +0300

    add jitpack and fix dependency id

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Корневой build.gradle.kts
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
